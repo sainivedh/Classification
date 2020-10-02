8b7d0ae4ed627f4af6c32ece9c06dc0395cb4df6
Sai Nivedh <39689508+sainivedh@users.noreply.github.com>
2020-10-03 03:04:15 +0530

    Add files via upload

--- Dogs_Cats_CNN/app.py
+++ Dogs_Cats_CNN/app.py
@@ -2,6 +2,9 @@ import streamlit as st
 import tensorflow
 from classify import predict
 from PIL import Image
+from io import BytesIO
+import cv2
+import numpy as np
 st.set_option('deprecation.showfileUploaderEncoding', False)
 # @st.cache
 # model = load_model
@@ -16,11 +19,14 @@ def main():
     st.markdown(html_temp,unsafe_allow_html=True)
 
     uploaded_file = st.file_uploader("Upload an image...",type=["jpg","png"])
-    #file = uploaded_file.read()
+
     if uploaded_file is not None:
+        file = uploaded_file.read()
+        file_bytes = np.asarray(bytearray(file), dtype=np.uint8)
+        img_cv2 = cv2.imdecode(np.fromstring(file, np.uint8), 1)
         img = Image.open(uploaded_file)
         st.image(img,caption='Uploaded Image.',use_column_width=True)
-        pred = predict(uploaded_file)
+        pred = predict(img_cv2)
         if pred == 1:
             st.success("Bow Bow, it's a DOG")
         else:
--- Dogs_Cats_CNN/classify.py
+++ Dogs_Cats_CNN/classify.py
@@ -1,14 +1,17 @@
 import tensorflow
 import numpy as np
 from tensorflow.keras.preprocessing import image
-
+import cv2
 
 model = tensorflow.keras.models.load_model('cnn_dogscats.hdf5')
 
 def predict(file):
-    test_img = image.load_img(file,target_size=(64,64))
-    test_img = image.img_to_array(test_img)
+    #test_img = image.load_img(file,target_size=(64,64))
+    #test_img = image.img_to_array(test_img)
+    #test_img = cv2.imread(file)
+    test_img = cv2.resize(file,(64,64))
     test_img = np.expand_dims(test_img,axis=0)
+    test_img = test_img
     result = model.predict(test_img)
     return result[0][0]
 
